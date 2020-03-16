7bc61119ec952e4d411ebba2f3d9dc621ba57bd8
Mark He <heke@hujiang.com>
2020-03-16 21:44:08 +0800

    移除背景，并设置背景透明

--- example/test-cases/model.js
+++ example/test-cases/model.js
@@ -18,9 +18,6 @@ export function renderModel(canvas, THREE) {
     camera.position.set(-5, 3, 10);
     camera.lookAt(new THREE.Vector3(0, 2, 0));
     scene = new THREE.Scene();
-    // scene.background = new THREE.Color(0xe0e0e0);
-    // scene.background = new THREE.Color(0xa0a0a0);
-    scene.fog = new THREE.Fog(0xe0e0e0, 20, 100);
     clock = new THREE.Clock();
     // lights
     var light = new THREE.HemisphereLight(0xffffff, 0x444444);
@@ -29,17 +26,6 @@ export function renderModel(canvas, THREE) {
     light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0, 20, 10);
     scene.add(light);
-    // ground
-    var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({
-      color: 0x999999,
-      depthWrite: false
-    }));
-    mesh.rotation.x = -Math.PI / 2;
-    // scene.add(mesh);
-    var grid = new THREE.GridHelper(200, 40, 0x000000, 0x000000);
-    grid.material.opacity = 0.2;
-    grid.material.transparent = true;
-    // scene.add(grid);
     // model
     var loader = new THREE.GLTFLoader();
     loader.load('models/RobotExpressive.glb', function (gltf) {
@@ -62,6 +48,7 @@ export function renderModel(canvas, THREE) {
     });
     renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio);
     renderer.setSize(canvas.width, canvas.height);
+    /// 设置背景透明
     renderer.setClearAlpha(0);
     renderer.gammaOutput = true;
     renderer.gammaFactor = 2.2;
